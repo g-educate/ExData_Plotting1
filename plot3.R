ceb7a54aa80d02ef14b8ece46729dd5d5fbd6a46
Vikram Manivasagam <vikram.manivasagam@centro.net>
2014-05-11 18:44:02 -0500

    Update to Plot 3

--- plot3.R
+++ plot3.R
@@ -1,10 +1,12 @@
 data3 <- read.table("dataset/household_power_consumption_subset.txt", sep = ";", header = TRUE)
 
+png(filename = "plot3.png", height = 480, width = 480)
+
 plot(data3$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(data3$Sub_metering_2, type = "l", col = "red")
 lines(data3$Sub_metering_3, type = "l", col = "blue")
 
 legend("topright", lty = 1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"))
 
-dev.copy(png, file = "plot3.png")
+# dev.copy(png, file = "plot33.png")
 dev.off()
