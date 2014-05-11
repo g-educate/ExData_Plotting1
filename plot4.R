4d2dc928d93a2d0fc6a56396d09211f7a9b96c68
Vikram Manivasagam <vikram.manivasagam@centro.net>
2014-05-11 18:33:26 -0500

    Finished Plot 4

---
+++ plot4.R
@@ -0,0 +1,17 @@
+data4 <- read.table("dataset/household_power_consumption_subset.txt", sep = ";", header = TRUE)
+
+par(mfrow = c(2, 2))
+
+plot(data2$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+
+plot(data2$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+
+plot(data4$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(data3$Sub_metering_2, type = "l", col = "red")
+lines(data3$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"))
+
+plot(data2$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_Power")
+
+dev.copy(png, file = "plot4.png")
+dev.off()
