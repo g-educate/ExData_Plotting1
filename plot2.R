2ec03deda475539b678034014f64532038384831
Vikram Manivasagam <vikram.manivasagam@centro.net>
2014-05-11 18:46:18 -0500

    Update to Plot 2

--- plot2.R
+++ plot2.R
@@ -1,6 +1,8 @@
 data2 <- read.table("dataset/household_power_consumption_subset.txt", sep = ";", header = TRUE)
 
+png(filename = "plot2.png", height = 480, width = 480)
+
 plot(data2$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 
-dev.copy(png, file = "plot2.png")
+# dev.copy(png, file = "plot2.png")
 dev.off()
