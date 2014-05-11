a635f97e9b2484fc075d553d4d7e0040137521e7
Vikram Manivasagam <vikram.manivasagam@centro.net>
2014-05-11 18:09:10 -0500

    Finished Plot 2

---
+++ plot2.R
@@ -0,0 +1,6 @@
+data2 <- read.table("dataset/household_power_consumption_subset.txt", sep = ";", header = TRUE)
+
+plot(data2$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot2.png")
+dev.off()
