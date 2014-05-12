a3bc7af8396ee487a7dae626a4be161a2b1e45b3
Vikram Manivasagam <vikram.manivasagam@centro.net>
2014-05-11 19:27:46 -0500

    Update to Data Preparation Script

--- data_preparation.R
+++ data_preparation.R
@@ -2,8 +2,11 @@ data <- read.table("dataset/household_power_consumption.txt", sep = ";", header 
 
 d <- as.Date(c("2007-02-01", "2007-02-02"), format = "%Y-%m-%d")
 
+data_subset <- subset(data, as.Date(data$Date, format = "%d/%m/%Y") >= d[1] & as.Date(data$Date, format = "%d/%m/%Y") <= d[2])
+data_subset <- cbind(data_subset, paste(data_subset$Date, data_subset$Time, sep = " "))
+
 write.table(
-    subset(data, as.Date(data$Date, format = "%d/%m/%Y") >= d[1] & as.Date(data$Date, format = "%d/%m/%Y") <= d[2]),
+    data_subset,
     file = "dataset/household_power_consumption_subset.txt",
     quote = FALSE,
     sep = ";"
